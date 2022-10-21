fc0fe51f61e4305436237e1ece1865186c2840e8
jaypostle <50502972+jaypostle@users.noreply.github.com>
2022-10-21 09:14:22 -0700

    final checklist created

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -43,19 +43,19 @@ const concerts = [
         id: uniqueId(), date: 'Monday, Sept 06 2021', venue: 'Ronald Lane', location: 'San Fransisco, CA',
     },
     {
-        id: uniqueId(), date: 'Monday, Sept 06 2021', venue: 'Ronald Lane', location: 'San Fransisco, CA',
+        id: uniqueId(), date: 'Tue Sept 21 2021 ', venue: 'Pier 3 East ', location: 'San Fransisco, CA',
     },
     {
-        id: uniqueId(), date: 'Monday, Sept 06 2021', venue: 'Ronald Lane', location: 'San Fransisco, CA',
+        id: uniqueId(), date: 'Fri Oct 15 2021 ', venue: 'View Lounge', location: 'San Fransisco, CA',
     },
     {
-        id: uniqueId(), date: 'Monday, Sept 06 2021', venue: 'Ronald Lane', location: 'San Fransisco, CA',
+        id: uniqueId(), date: 'Sat Nov 06 2021 ', venue: 'Hyatt Agency', location: 'San Fransisco, CA',
     },
     {
-        id: uniqueId(), date: 'Monday, Sept 06 2021', venue: 'Ronald Lane', location: 'San Fransisco, CA',
+        id: uniqueId(), date: 'Fri Nov 26 2021', venue: 'Moscow Center', location: 'San Fransisco, CA',
     },
     {
-        id: uniqueId(), date: 'Monday, Sept 06 2021', venue: 'Ronald Lane', location: 'San Fransisco, CA',
+        id: uniqueId(), date: 'Wed Dec 15 2021', venue: 'Press Club ', location: 'San Fransisco, CA',
     },
 ]
 
